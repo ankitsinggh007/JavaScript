ac3b98fcf80ca8e0c8019e9d8749a8cafc7c0f14
ankitsinggh007 <as3824115@gmail.com>
2025-01-12 15:57:45 +0530

    merge sort

--- DSA/Sorting/insertion-sort.js
+++ DSA/Sorting/insertion-sort.js
@@ -8,25 +8,36 @@
 // 2. take the next element and insert it into the correct position in the sorted array
 // 3. repeat the same process for the remaining elements 
 
-function selectionSort(arr) {
-        
-    for(let i=0;i<arr.length;i++){
-        
-        let minIndex=i;
-        
-        for(let j=i;j<arr.length;j++){
-            if(arr[minIndex]>arr[j]){
-                minIndex=j;
-            }
+function insertionSort(arr) {
+   for(let i=1;i<arr.length;i++){
+    let j=i;
+
+    let size=j-1;
+    while(size>=0){
+        if(arr[size]>arr[j]){
+            let temp=arr[size];
+            arr[size]=arr[j];
+            arr[j]=temp;
+            size--;
+            j--;
         }
-        let temp =arr[i]
-        arr[i]=arr[minIndex]
-        arr[minIndex]=temp;
+        else{
+            break;
+        }
+
     }
+
+
+   }
+   return arr;      
+   
 }
 
-// Time complexity: O(n^2)  time complexity is always O(n^2) no matter what the input is   
-// Space complexity: O(1)   time complexity is always O(n^2) no matter what the input is    
-// stable: No
+console.log(insertionSort([5,4,3,2,1]));
+console.log(insertionSort([5,4,3,2,1,2,3,4,5]));
+
+// Time complexity: O(n^2)  time complexity is always O(n^2)
+// Space complexity: O(1) time complexity is always O(n^2)
+// stable: Yes  
 // In-place: Yes
 
