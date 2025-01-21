9c40bd762165b514ce290c2fa04ea872ffa19e96
Ankit singh <as3824115@gmail.com>
2025-01-21 19:01:22 +0530

    day-08

--- DSA/Array/array-medium.js
+++ DSA/Array/array-medium.js
@@ -184,3 +184,32 @@ let array=[];
                  [3,2,1] is [1,2,3]
  */
 console.log("solution-6")
+
+var nextPermutation = function (nums) {
+    let n = nums.length;
+    let i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+        i--;
+    }
+
+    if (i >= 0) {
+       
+        let j = n - 1;
+        while (j > i && nums[j] <= nums[i]) {
+            j--;
+        
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    let left = i + 1,
+        right = n - 1;
+    while (left < right) {
+        [nums[left], nums[right]] = [nums[right], nums[left]];
+        left++;
+        right--;
+    }
+
+    return nums;
+};
+
+}
