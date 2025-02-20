04e72455efebcb9e76fb7a3ae14c4c7df5a96652
Ankit singh <as3824115@gmail.com>
2025-02-21 01:02:18 +0530

    Kth Missing Positive Number

--- DSA/Binary-search/1d-array.js
+++ DSA/Binary-search/1d-array.js
@@ -538,3 +538,25 @@ function isMinWeight(weights, min, days) {
     return true;
 
 }
+/*
+1539. Kth Missing Positive Number
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Return the kth positive integer that is missing from this array.
+*/
+console.log("question-15");
+var findKthPositive = function(arr, k) {
+    let low = 0, high = arr.length - 1;
+    
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let missing = arr[mid] - (mid + 1); 
+        
+        if (missing < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return low + k;  
+};
