a7d7b1757eb952b0e760ea19107b5cdc91854a85
ankitsinggh007 <as3824115@gmail.com>
2025-03-06 23:42:46 +0530

    mes

--- DSA/Binary-search/1d-array.js
+++ DSA/Binary-search/1d-array.js
@@ -661,4 +661,4 @@ function check(nums, mid, k) {
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 The overall run time complexity should be O(log (m+n)).
 */
- console.log('question-19')
+ console.log('question-20')
