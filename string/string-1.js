c79ba5ac48a6c0f3b061d7de832f4466424cba24
Ankit singh <as3824115@gmail.com>
2025-03-11 02:07:37 +0530

    Largest Odd Number in String

--- string/string-1.js
+++ string/string-1.js
@@ -41,3 +41,16 @@ var reverseWords = function(s) {
 
 
 };
+/*
+903. Largest Odd Number in String
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+A substring is a contiguous sequence of characters within a string.
+*/
+var largestOddNumber = function(num) {
+    for(let i=num.length-1;i>=0;i--){
+
+               if(num[i]%2==0) continue;
+               else return num.slice(0,i+1);
+       }
+       return "";
+};
