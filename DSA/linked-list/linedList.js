7b81831a51fa33aa1c2094d5788cc69f1f1aa54c
Ankit singh <as3824115@gmail.com>
2025-03-19 13:20:50 +0530

    add node in dll

--- DSA/linked-list/linedList.js
+++ DSA/linked-list/linedList.js
@@ -1,52 +1,51 @@
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    }
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
 }
 
 class LinkedList {
-    constructor() {
-        this.head = null;
+  constructor() {
+    this.head = null;
+  }
+
+  insertAtEnd(x) {
+    let newNode = new Node(x);
+
+    // If the list is empty
+    if (!this.head) {
+      this.head = newNode; // New node becomes the head
+      return this.head;
     }
 
-    insertAtEnd(x) {
-        let newNode = new Node(x);
-
-        // If the list is empty
-        if (!this.head) {
-            this.head = newNode;  // New node becomes the head
-            return this.head;
-        }
-
-        let temp = this.head;
-        while (temp.next !== null) {
-            temp = temp.next;
-        }
-        temp.next = newNode;  // Insert at the end
-
-        return this.head;
+    let temp = this.head;
+    while (temp.next !== null) {
+      temp = temp.next;
     }
-    getCount(head) {
-        
-        let len=0;
-        let temp=head;
-        while(temp!=null){
-            len++;
-            temp=temp.next;
-        }
-        return len;
-    }
+    temp.next = newNode; // Insert at the end
 
-    print() {
-        let temp = this.head;
-        let result = "";
-        while (temp) {
-            result += `${temp.data} → `;
-            temp = temp.next;
-        }
-        console.log(result + "null");
+    return this.head;
+  }
+  getCount(head) {
+    let len = 0;
+    let temp = head;
+    while (temp != null) {
+      len++;
+      temp = temp.next;
     }
+    return len;
+  }
+
+  print() {
+    let temp = this.head;
+    let result = "";
+    while (temp) {
+      result += `${temp.data} → `;
+      temp = temp.next;
+    }
+    console.log(result + "null");
+  }
 }
 
 /*
@@ -66,12 +65,12 @@ The output will be the entire list after calling your function.
  
 */
 
-var deleteNode = function(node) {
-    // Copy the next node's value into the current node
-    node.val = node.next.val;  // 🔥 Use `val` instead of `data`
+var deleteNode = function (node) {
+  // Copy the next node's value into the current node
+  node.val = node.next.val; // 🔥 Use `val` instead of `data`
 
-    // Skip over the next node
-    node.next = node.next.next;
+  // Skip over the next node
+  node.next = node.next.next;
 };
 
 /*
@@ -82,17 +81,66 @@ Given a singly linked list. The task is to find the length of the linked list, w
 Search in Linked List
 Given a linked list of n nodes and a key, the task is to check if the key is present in the linked list or not.
 */
-const searchKey=(n, head, key)=> {
-    let count=0;
-    let temp=head;
-    while( count !== n){
-            
-            count++;
-            
-            if(temp.data==key) return true;
-            temp=temp.next;
-        
-    }
-    
-    return false;
+const searchKey = (n, head, key) => {
+  let count = 0;
+  let temp = head;
+  while (count !== n) {
+    count++;
+
+    if (temp.data == key) return true;
+    temp = temp.next;
+  }
+
+  return false;
+};
+
+class Node {
+  constructor(data, next = null, prev = null) {
+    this.data = data;
+    this.next = null;
+    this.prev = null;
+  }
 }
+
+class DLL {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+/*
+Given a doubly-linked list, a position p, and an integer x. The task is to add a new node with value x at 
+the position just after pth node in the doubly linked list and return the head of the updated list.
+*/
+// class Node{
+//     constructor(data){
+//         this.data = data;
+//         this.next = null;
+//         this.prev = null;
+//     }
+// }
+function addNode(head, p, x) {
+  let temp = head;
+  if (!temp) return head;
+  let count = 1;
+  while (temp != null && count !== p) {
+    temp = temp.next;
+    count++;
+  }
+  if (!temp) return head;
+  let newNode = new Node(x);
+  let upcoming = temp.next;
+
+
+  newNode.next = upcoming;
+  newNode.prev = temp;
+  temp.next = newNode;
+  if (upcoming) {
+    upcoming.prev = newNode;
+  }
+  return head;
+}
+
+func
