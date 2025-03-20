6d8a4847c6889ee6b94c00dc1d13816c7d29d2e9
ankitsinggh007 <as3824115@gmail.com>
2025-03-20 20:58:23 +0530

    reverse the ll

--- DSA/linked-list/linedList.js
+++ DSA/linked-list/linedList.js
@@ -227,3 +227,19 @@ var middleNode = function(head) {
     return turtle;
     
 };
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+var reverseList = function(head) {
+    let prev = null;
+    let current = head;
+    
+    while (current !== null) {
+        let next = current.next; 
+        current.next = prev;     
+        prev = current;          
+        current = next;          
+    }
+    
+    return prev; // New head of the reversed list
+};
