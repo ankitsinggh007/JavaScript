75772b71e1ba156ff97b77d003af6e41fe91881a
ankitsinggh007 <as3824115@gmail.com>
2025-03-23 19:26:43 +0530

    Linked List Cycle II

--- DSA/linked-list/linedList.js
+++ DSA/linked-list/linedList.js
@@ -283,3 +283,36 @@ var hasCycle = function(head) {
   }
   return false;
 };
+/*
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. 
+Note that pos is not passed as a parameter.
+Do not modify the linked list.
+*/
+var detectCycle = function(head) {
+
+
+  let slow=head;
+  let fast=head;
+
+  while(fast!=null && fast.next!=null){
+
+      slow=slow.next;
+      fast=fast.next.next;
+      if(slow==fast) {
+          slow=head;
+          break;
+      }
+
+  }
+  if(fast==null || fast.next==null) return null;
+
+  while(true){
+      if(fast==slow) return slow;
+
+      slow=slow.next;
+      fast=fast.next;
+  }
+            
+};
