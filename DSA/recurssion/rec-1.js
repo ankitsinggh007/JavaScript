0059be8f2757e0d498ab3cf17a72c5cd6986e054
ankitsinggh007 <as3824115@gmail.com>
2025-04-07 12:48:52 +0530

    Generate all binary strings of size k

--- DSA/recurssion/rec-1.js
+++ DSA/recurssion/rec-1.js
@@ -81,3 +81,64 @@ function modPow(base, exp) {
     }
     return result;
 }
+// Given an integer, K. Generate all binary strings of size k without consecutive 1’s.
+// A utility function generate all string without
+// consecutive 1'sof size K
+function generateAllStringsUtil(K, str, n)
+{
+
+    // Print binary string without consecutive 1's
+    if (n == K) {
+
+        // Terminate binary string
+        str[n] = "\0";
+        console.log(str.join("") + " ");
+        return;
+    }
+
+    // If previous character is '1' then we put
+    // only 0 at end of string
+    // example str = "01" then new string be "010"
+    if (str[n - 1] == "1") {
+        str[n] = "0";
+        generateAllStringsUtil(K, str, n + 1);
+    }
+
+    // If previous character is '0' than we put
+    // both '1' and '0' at end of string
+    // example str = "00" then
+    // new string "001" and "000"
+    if (str[n - 1] == "0") {
+        str[n] = "0";
+        generateAllStringsUtil(K, str, n + 1);
+        str[n] = "1";
+        generateAllStringsUtil(K, str, n + 1);
+    }
+}
+
+// Function generate all binary string without
+// consecutive 1's
+function generateAllStrings(K)
+{
+    // Base case
+    if (K <= 0)
+        return;
+
+    // One by one stores every
+    // binary string of length K
+    var str = new Array(K);
+
+    // Generate all Binary string
+    // starts with '0'
+    str[0] = "0";
+    generateAllStringsUtil(K, str, 1);
+
+    // Generate all Binary string
+    // starts with '1'
+    str[0] = "1";
+    generateAllStringsUtil(K, str, 1);
+}
+
+/* Driver code */
+var K = 3;
+generateAllStrings(K);
