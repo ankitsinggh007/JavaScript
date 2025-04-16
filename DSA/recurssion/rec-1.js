9a213b41a9b2dd02d47ea0f6e449ca4cbd36baa2
ankitsinggh007 <as3824115@gmail.com>
2025-04-16 22:25:25 +0530

    do some modification

--- DSA/recurssion/rec-1.js
+++ DSA/recurssion/rec-1.js
@@ -245,7 +245,7 @@ Return the number of non-empty subsequences of nums such that the sum of
 the minimum and maximum element on it is less or equal to target.
 Since the answer may be too large, return it modulo 109 + 7.
 */
-let MOD = 10e9 + 7;
+let MOD = 10**9 + 7;
 
 const modPow = (base, exp, mod) => {
     let result = 1;
