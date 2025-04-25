2af5374040b115a689162cae76631f8c1623b234
ankitsinggh007 <as3824115@gmail.com>
2025-04-25 21:50:27 +0530

    Container with most water

--- DSA/pattern-wise/two-pointer.js
+++ DSA/pattern-wise/two-pointer.js
@@ -173,3 +173,27 @@ var sortedSquares = function(nums) {
         }
         return arra;
 };
+/*
+11. Container With Most Water
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+Return the maximum amount of water a container can store.
+Notice that you may not slant the container.
+*/
+var maxArea = function(height) {
+    
+    let i=0;
+    let j=height.length-1;
+    max=(j)*Math.min(height[i],height[j]);
+    while(i<j){
+       
+        if(height[i]<height[j]){
+            i++;
+        }else{
+            j--;
+        }
+         let currentMax=(j-i)*Math.min(height[i],height[j]);
+       max = max<currentMax?currentMax:max;
+    }
+    return max;
+};
