c23f4fe0b09ccdcd0e706f867b2d93f367a97205
ankitsinggh007 <as3824115@gmail.com>
2025-06-11 00:46:45 +0530

    made some changes

--- DSA/pattern-wise/recurrsion.js
+++ DSA/pattern-wise/recurrsion.js
@@ -1,6 +1,23 @@
 // recursion notes
 
+/*
+CORE PRINCIPLES: 1. Fix Perception First:
+- Don't blindly minimize input. Instead, solve via decision-making first.
+- Use recursion trees to visualize choices and outcomes.
+- Mimic mathematical induction: Base Case Induction Extra Processing.
+2. When to Use Recursion:
+a. If problem asks for all combinations/choices Decision-based recursion b. If smaller input leads to solution Input-reduction based recursion
+c. If recursion is explicitly required Handle as recursive
+3. Two Core Approaches:
+Decision-making (Tree-based): Used in subsets, permutations, combinations. Base-Induction-Hypothesis: Used in factorial, sum, reverse, divide & conquer.
+4. General Steps to Solve: - Identify input/output
+- Choose between decision-tree vs B-I-H model
+- Draw recursion tree if decision-based
+- Write base case (minimum valid input)
+- Use hypothesis: assume small input works
+- Add extra work depending on input/output requirement - Code last
 
+*/
 
 
 
@@ -15,12 +32,12 @@
 You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
 */
-var climbStairs = function(n) {
-        let arr=[]
-    function helper(n){
-        if(n==1||n==0) return 1;
-    if(arr[n]) return arr[n];
-        arr[n]= helper(n-1) + helper(n-2)
+var climbStairs = function (n) {
+    let arr = []
+    function helper(n) {
+        if (n == 1 || n == 0) return 1;
+        if (arr[n]) return arr[n];
+        arr[n] = helper(n - 1) + helper(n - 2)
         return arr[n];
     }
     return helper(n);
@@ -29,54 +46,54 @@ var climbStairs = function(n) {
 Given a string s, you can transform every letter individually to be lowercase or uppercase to create another string.
 Return a list of all possible strings we could create. Return the output in any order.
 */
-var letterCasePermutation = function(s) {
-    let ans=[];
-    function helper(i=0,res=""){
-        if(res.length==s.length){
+var letterCasePermutation = function (s) {
+    let ans = [];
+    function helper(i = 0, res = "") {
+        if (res.length == s.length) {
             ans.push(res);
             return;
         }
-        
-        if("a"<=s[i] && s[i]<="z" || "A"<=s[i] && s[i]<="Z"){
-             res+=s[i].toUpperCase()
-            helper(i+1,res);
-            res-=s[i].toUpperCase()
-            helper(i+1,res);
+
+        if ("a" <= s[i] && s[i] <= "z" || "A" <= s[i] && s[i] <= "Z") {
+            res += s[i].toUpperCase()
+            helper(i + 1, res);
+            res -= s[i].toUpperCase()
+            helper(i + 1, res);
         }
-        else{
-             res+=s[i];
-            helper(i+1,res);
+        else {
+            res += s[i];
+            helper(i + 1, res);
         }
     }
-    
+
     helper();
     return ans;
-    
+
 };
 // alternate with some changes 
 /**
  * @param {string} s
  * @return {string[]}
  */
-var letterCasePermutation = function(s) {
-let ans=[];
-  function helper(i=0,str=""){
-    if(i>=s.length){
-        ans.push(str);
-        return;
-    }
+var letterCasePermutation = function (s) {
+    let ans = [];
+    function helper(i = 0, str = "") {
+        if (i >= s.length) {
+            ans.push(str);
+            return;
+        }
 
-    // if char make two call
-    if("a"<=s[i]&& s[i]<="z" || "A"<=s[i] && s[i]<="Z"){
-        helper(i+1,str+s[i].toUpperCase());
-        helper(i+1,str+s[i].toLowerCase());
-    }else{
-        helper(i+1,str+s[i]);
-    }
+        // if char make two call
+        if ("a" <= s[i] && s[i] <= "z" || "A" <= s[i] && s[i] <= "Z") {
+            helper(i + 1, str + s[i].toUpperCase());
+            helper(i + 1, str + s[i].toLowerCase());
+        } else {
+            helper(i + 1, str + s[i]);
+        }
 
-  }  
-  helper()
-  return ans;
+    }
+    helper()
+    return ans;
 };
 /*
 Given a string s, partition s such that every substring of the partition is a palindrome. 
