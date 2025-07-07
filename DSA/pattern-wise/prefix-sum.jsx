7f5907b80b15ed846a96c10eb57adf10c20103b5
ankitsinggh007 <as3824115@gmail.com>
2025-07-07 17:58:49 +0530

    find # of subarray having sum of k

--- DSA/pattern-wise/prefix-sum.jsx
+++ DSA/pattern-wise/prefix-sum.jsx
@@ -1,5 +1,24 @@
+// find # of subarray having sum of k.
+var subarraySum = function(nums, k) {
+   //store prefix sum first
+    let prefixSum=new Map([[0,1]]);
+    let count=0;
+    let sum=0;
+    for(let i =0;i<nums.length;i++){
+            sum+=nums[i];
+            let value=sum-k;
+            if(prefixSum.has(value)){
+                count+=prefixSum.get(value);
+            }
+                prefixSum.set(sum,(prefixSum.get(sum)||0)+1);
 
 
+            
+
+    }
+    return count ;
+
+};
 // find max length of subarray having sum == k
 var maxSubArrayLen = function(nums, k) {
     let mp=new Map([[0,-1]]);
