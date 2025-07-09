b6a049270a6e11627126044552617e193635e9bf
ankitsinggh007 <as3824115@gmail.com>
2025-07-09 22:01:16 +0530

    maxiumum subarray sum

--- DSA/pattern-wise/prefix-sum.jsx
+++ DSA/pattern-wise/prefix-sum.jsx
@@ -81,3 +81,15 @@ var findMaxLength = function(nums) {
     return maxLength;
 
 };
+
+//maxiumu subarray sum 
+var maxSubArray = function (nums) {
+    let sum = nums[0];
+    let max = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+       sum=Math.max(sum+nums[i],nums[i])
+
+        max = Math.max(sum, max);
+    }
+    return max;
+};
