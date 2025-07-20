cb6cb5440120bdbc80223e2e890722d43560d844
ankitsinggh007 <as3824115@gmail.com>
2025-07-20 14:28:12 +0530

    Next greatest element

--- DSA/pattern-wise/stack.jsx
+++ DSA/pattern-wise/stack.jsx
@@ -22,6 +22,7 @@ let proto={
     
     // ------------------------------------------------------------------------------------------
     
+    // balanced paranthesis 
 var isValid = function(s) {
     let arr=[];
 
@@ -46,4 +47,24 @@ var isValid = function(s) {
 
     return (arr.length)!=0 ?false:true;
 
+};
+
+//Next greatest Element
+var nextGreaterElement = function (nums1, nums2) {
+    let arr = [];
+    let mp = new Map();
+    for (let i = nums2.length - 1; i >= 0; i--) {
+        while (arr.length != 0 && nums2[i] > arr[arr.length - 1]) {
+            arr.pop();
+        }
+
+        mp.set(nums2[i], arr.length === 0 ? -1 : arr[arr.length - 1]);
+        arr.push(nums2[i]);
+    }
+
+    let ans = [];
+    for (let i = 0; i < nums1.length; i++) {
+        ans[i] = mp.get(nums1[i]);
+    }
+    return ans;
 };
