1ee4f3146aed185d911dce4cc82283733d9eb71a
ankitsinggh007 <as3824115@gmail.com>
2025-07-23 14:32:10 +0530

    optimized solution for max rectangle in histogram

--- DSA/pattern-wise/stack.jsx
+++ DSA/pattern-wise/stack.jsx
@@ -142,4 +142,27 @@ var nextGreaterElements = function (nums) {
             return maxHeights;
             
 };
+// optimized
+function largestRectangleArea(heights) {
+    let stack = [];
+    let maxArea = 0;
 
+    for (let i = 0; i < heights.length; i++) {
+        let start = i;
+
+        while (stack.length && stack[stack.length - 1][1] > heights[i]) {
+            let [index, height] = stack.pop();
+            maxArea = Math.max(maxArea, height * (i - index));
+            start = index;
+        }
+
+        stack.push([start, heights[i]]);
+    }
+
+    while (stack.length) {
+        let [index, height] = stack.pop();
+        maxArea = Math.max(maxArea, height * (heights.length - index));
+    }
+
+    return maxArea;
+}
