a5f1cd82bf4955105e5945e1202e28fec6005fc0
ankitsinggh007 <as3824115@gmail.com>
2025-07-26 20:23:20 +0530

    inorderTraversal

--- DSA/pattern-wise/Binarytree.jsx
+++ DSA/pattern-wise/Binarytree.jsx
@@ -95,3 +95,21 @@ var zigzagLevelOrder = function (root) {
     return ans;
 };
 console.log(zigzagLevelOrder(myTree),"zigzag traversal");
+
+//inroder Traversal
+
+var inorderTraversal = function(root) {
+    let ans=[];
+    let stack=[];
+    let curr=root;
+    while(curr||stack.length){
+        while(curr){
+            stack.push(curr);
+            curr=curr.left;
+        }
+        curr=stack.pop();
+        ans.push(curr.val);
+        curr=curr.right
+    }
+    return ans;
+};
