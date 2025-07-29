6670d2e0b5581bbc5642078e1a50250bbca5fb55
ankitsinggh007 <as3824115@gmail.com>
2025-07-29 13:23:50 +0530

    LCA of B.T

--- DSA/pattern-wise/Binarytree.jsx
+++ DSA/pattern-wise/Binarytree.jsx
@@ -260,3 +260,17 @@ function rightView(root){
         }
         return ans;
 }
+//LCA of Binary tree
+var lowestCommonAncestor = function(root, p, q) {
+
+    if(!root) return null;
+    if(root==p || root==q) return root;
+    
+    let left=lowestCommonAncestor(root.left,p,q);
+    let right=lowestCommonAncestor(root.right,p,q);
+
+    if(left && right) return root;
+    if(left) return left
+    if(right) return right
+
+};
