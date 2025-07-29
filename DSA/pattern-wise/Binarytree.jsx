c72094ce437dc09484825e7a197fa8a26b36f2bd
ankitsinggh007 <as3824115@gmail.com>
2025-07-29 13:49:01 +0530

    LCA in BST

--- DSA/pattern-wise/Binarytree.jsx
+++ DSA/pattern-wise/Binarytree.jsx
@@ -280,10 +280,7 @@ var lowestCommonAncestor = function(root, p, q) {
 if(!root) return null;
 if(root==p||root==q) return root;
 if((root.val>p.val && root.val<q.val) ||(root.val<p.val && root.val>q.val)) return root;
-
-let left= lowestCommonAncestor(root.left,p,q)
-let right= lowestCommonAncestor(root.right,p,q)
-    if(left && right) return root;
-    if(left) return left;
-    if(right) return right;
+// pand q is in right
+if(root.val>p.val && root.val>q.val) return lowestCommonAncestor(root.left,p,q)
+if(root.val<p.val && root.val<q.val) return lowestCommonAncestor(root.right,p,q)
 };
