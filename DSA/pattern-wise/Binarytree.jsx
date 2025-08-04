54ee0c178d254e6d5718801fac0e816ecdbce4cd
ankitsinggh007 <as3824115@gmail.com>
2025-08-04 06:23:18 +0530

    Search in BST

--- DSA/pattern-wise/Binarytree.jsx
+++ DSA/pattern-wise/Binarytree.jsx
@@ -431,3 +431,15 @@ var isValidBST = function(root) {
     }
     return helper(root);
 };
+//Search in BST
+var searchBST = function (root, val) {
+
+    if (!root) return null;
+    if (root.val == val) return root;
+
+    if (root.val < val)
+        return searchBST(root.right, val);
+    else
+        return searchBST(root.left, val);
+
+};
