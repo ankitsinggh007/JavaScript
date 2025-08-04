7bc0f950d302933c3528b37943b8dceb5c93a36a
ankitsinggh007 <as3824115@gmail.com>
2025-08-04 06:17:50 +0530

    Is Valid BST

--- DSA/pattern-wise/Binarytree.jsx
+++ DSA/pattern-wise/Binarytree.jsx
@@ -419,3 +419,15 @@ var insertIntoBST = function(root, val) {
     return root;
 
 };
+
+// Is Valid BST
+var isValidBST = function(root) {
+    function helper(root,min=-Infinity,max=Infinity){
+        if(!root) return true;
+        if( root.val<=min || root.val>=max ) return false;
+
+        return helper(root.left,min,root.val) &&
+        helper(root.right,root.val,max);
+    }
+    return helper(root);
+};
