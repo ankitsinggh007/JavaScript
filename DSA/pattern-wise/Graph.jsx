f6d161a0792c8ff9b2bbb241343a5729ee60c51e
ankitsinggh007 <as3824115@gmail.com>
2025-08-14 21:04:48 +0530

    Clone graph using dfs

--- DSA/pattern-wise/Graph.jsx
+++ DSA/pattern-wise/Graph.jsx
@@ -172,3 +172,34 @@ var orangesRotting = function (grid) {
     }
     return fresh === 0 ? time : -1;
 };
+// clone graph 
+/**
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {_Node} node
+ * @return {_Node}
+ */
+var cloneGraph = function(node) {
+    
+        let mp=new Map();
+    
+
+    function dfs(node){
+        if(!node) return null;
+        if(mp.has(node)) return mp.get(node);
+    let newNode=new Node(node.val);
+    mp.set(node,newNode);
+
+    for(let nei of node.neighbors){
+        newNode.neighbors.push(dfs(nei));
+    }
+    return newNode;
+    }
+    return dfs(node);
+};
