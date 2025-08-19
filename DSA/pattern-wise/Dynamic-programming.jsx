964c597553a92e13a851bec123aa8856d0729729
ankitsinggh007 <as3824115@gmail.com>
2025-08-19 13:31:57 +0530

    house robber- I using DP

--- DSA/pattern-wise/Dynamic-programming.jsx
+++ DSA/pattern-wise/Dynamic-programming.jsx
@@ -10,3 +10,40 @@ function fibonacciMemo(n, memo = {}) {
   memo[n] = fibonacciMemo(n - 1, memo) + fibonacciMemo(n - 2, memo);
   return memo[n];
 }
+// fibonacci with tabulation
+function fibonacciTab(n) {
+    if (n <= 1) return n;
+    const dp = new Array(n + 1).fill(0);
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+    }   
+//house robber
+function houseRobber(nums){
+    let dp=Array(nums.length).fill(-1);
+    function rob(n){
+        if(n==0) return nums[0];
+        if(n<0) return 0;
+        if(dp[n]!==-1) return dp[n];
+        let include=nums[n]+rob(n-2);
+        let exclude=rob(n-1);
+        dp[n]=Math.max(include,exclude);
+        return dp[n];
+
+    }
+}
+
+//house robber with tabulation
+function houseRobberTab(nums) {
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+    const dp = Array(nums.length).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+    }
+    return dp[nums.length - 1];
+}
