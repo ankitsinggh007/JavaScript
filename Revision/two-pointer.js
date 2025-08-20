fecced9ef9041ca6a8361aba42d956d7686c68e6
ankitsinggh007 <as3824115@gmail.com>
2025-08-21 00:42:07 +0530

    two-pointer problem set

--- Revision/two-pointer.js
+++ Revision/two-pointer.js
@@ -10,16 +10,16 @@ Constraints:
 
 -1e9 <= nums[i], target <= 1e9 (non-decreasing order)
 */
-function pairSum(nums,target){
-    let [i,j]=[0,nums.length-1];
-    while(i<j){
-        let d=nums[i] + nums[j];
-        if(d===target){
-            return [i,j];
-        }else if(d<target){
+function pairSum(nums, target) {
+    let [i, j] = [0, nums.length - 1];
+    while (i < j) {
+        let d = nums[i] + nums[j];
+        if (d === target) {
+            return [i, j];
+        } else if (d < target) {
             i++;
         }
-        else{
+        else {
             j--;
         }
     }
@@ -34,14 +34,14 @@ Examples:
 "race a car" → false
 */
 //
-function checkPlaindrom(s){
-    
-    let str=s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    let [l,r]=[0,str.length-1];
+function checkPlaindrom(s) {
 
-    while(l<r){
-        if(str[l]!==str[r]) return false;
-        else{
+    let str = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let [l, r] = [0, str.length - 1];
+
+    while (l < r) {
+        if (str[l] !== str[r]) return false;
+        else {
             l++;
             r--;
         }
@@ -49,19 +49,19 @@ function checkPlaindrom(s){
     return true;
 }
 //max water container
-var maxArea = function(height) {
-    
-    let maxWater=0;
+var maxArea = function (height) {
 
-    let [l,r]=[0,height.length-1];
+    let maxWater = 0;
 
-    while(l<r){
+    let [l, r] = [0, height.length - 1];
 
-         maxWater=Math.max(Math.min(height[l],height[r])*(r-l),maxWater);
-        if(height[l]<height[r]){
+    while (l < r) {
+
+        maxWater = Math.max(Math.min(height[l], height[r]) * (r - l), maxWater);
+        if (height[l] < height[r]) {
             l++;
         }
-        else{
+        else {
             r--;
         }
 
@@ -71,20 +71,78 @@ var maxArea = function(height) {
 };
 //283. Move Zeroes
 
-var moveZeroes = function(nums) {
-    
-    let read=0;
-    let write=0
+var moveZeroes = function (nums) {
 
-    for(read;read<nums.length;read++){
-        if(nums[read]!=0){
-            nums[write++]=nums[read];
+    let read = 0;
+    let write = 0
+
+    for (read; read < nums.length; read++) {
+        if (nums[read] != 0) {
+            nums[write++] = nums[read];
         }
 
     }
-    while(write<nums.length){
-        nums[write++]=0;
+    while (write < nums.length) {
+        nums[write++] = 0;
     }
-    
+
+
+};
+//practice set //
+// 977 leetcode
+
+
+
+function sortedSquares(nums) {
+
+    let arr = new Array(nums.length);
+
+    let [l, r] = [0, nums.length - 1];
+    let last = nums.length - 1;
+    while (l < r) {
+        if (Math.abs(nums[l]) < Math.abs(nums[r])) {
+            arr([last--] = nums)[r];
+            r--;
+        } else if (Math.abs(nums[l]) > Math.abs(nums[r])) {
+            arr[last--] = nums[l];
+            l++;
+        }
+        else {
+            arr[last--] = nums[r];
+            arr[last--] = nums[r];
+            r--;
+            l++;
+        }
+    }
+    arr[last] = nums[l];
+
+    arr = arr.map((ele) => ele * ele);
+    return arr;
+};
+//palindrom-2
+
+function helper(s,l,r){
+
+    while(l<r){
+        if(s[l]!==s[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+
+var validPalindrome = function(s) {
+    let [l,r]=[0,s.length-1];
+    while(l<r){
+        if(s[l]!=s[r] ){
+            return helper(s,l+1,r)||  helper(s,l,r-1)    
+        }
+
+        l++;
+        r--;
+    }
+    return true;
+
+
 
 };
