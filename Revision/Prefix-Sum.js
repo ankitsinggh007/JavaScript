2044acc25201d1b9a3f2b6ca32437f796a191170
ankitsinggh007 <as3824115@gmail.com>
2025-08-28 12:26:15 +0530

    subarrayDivisibleByK

--- Revision/Prefix-Sum.js
+++ Revision/Prefix-Sum.js
@@ -84,5 +84,40 @@ function maxSubArray(nums) {
         maxSum=Math.max(maxSum,currSum);
         if(currSum<0)currSum=0;
     }
-
 }
+/*
+Problem (classic style):
+
+Given an integer array nums and an integer k, return the number of (non-empty) subarrays that have a sum divisible by k.
+
+Example 1:
+
+Input: nums = [4,5,0,-2,-3,1], k = 5
+Output: 7
+Explanation: There are 7 subarrays with sum divisible by 5:
+[4,5,-2,-3,1], [5], [5,0], [0], [0,-2,-3], [-2,-3], [-2,-3,1]
+*/
+function subarrayDivisibleByK(nums,k){
+    let currSum=0;
+    let count=0;
+    let mp=new Map();
+    mp.set(0,1);
+
+    for(let i=0;i<nums.length;i++){
+        currSum+=nums[i];
+        let mod=((currSum%k)+k)%k;
+        if(mp.has(mod)){
+            count+=mp.get(mod);
+            mp.set(mod,mp.get(mod)+1);
+        }
+        else
+            mp.set(mod,1);
+
+    }
+    console.log(count);
+    return count;
+}
+
+
+subarrayDivisibleByK([4,5,0,-2,-3,1],5);
+
