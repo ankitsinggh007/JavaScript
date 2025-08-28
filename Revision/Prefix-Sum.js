c346cd362a5348fd296faae9eaf5d88daba90185
ankitsinggh007 <as3824115@gmail.com>
2025-08-28 09:54:59 +0530

    SubarrayPresent

--- Revision/Prefix-Sum.js
+++ Revision/Prefix-Sum.js
@@ -26,3 +26,38 @@ function subarrayCountWithKSum(nums, k) {
 
 }
 subarrayCountWithKSum([1, 1, 1], 2);
+/*
+Problem:
+Given an integer array nums of length n, and an integer target, 
+return true if there exists a continuous subarray of length at least 2 that sums to target. 
+Otherwise, return false.
+
+Example 1:
+
+Input: nums = [23, 2, 4, 6, 7], target = 6
+Output: true
+Explanation: [2,4] or [4,6] sums to 6.
+*/
+
+function subarrayPresent(nums, k) {
+    let currSum = 0;
+    let mp=new Map();
+        mp.set(0,-1);
+
+    for (let i = 0; i < nums.length; i++) {
+
+        currSum += nums[i];
+        if (mp.has(currSum - k)) {
+                let j= mp.get(currSum-k);
+                if(i-j>1) return true;
+                
+        }
+        if(!mp.has(currSum))
+            mp.set(currSum,i);
+
+    }
+
+    return false;
+
+
+}
