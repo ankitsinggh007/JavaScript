628c1bf7127430501507b62495399c902f14e039
ankitsinggh007 <as3824115@gmail.com>
2025-09-28 09:31:19 +0530

    Lowest common ancestor in binary tree

--- Revision/Tree.js
+++ Revision/Tree.js
@@ -436,3 +436,18 @@ var maxPathSum = function(root) {
   }
   return dfs(root)[1];
 };
+/*
+LCA in binary tree
+*/
+var lowestCommonAncestor = function(root, p, q) {
+        if(!root) return null;    
+        if(root==p||root==q) return root;
+
+        let left=lowestCommonAncestor(root.left,p,q);
+        let right=lowestCommonAncestor(root.right,p,q);
+        
+        if(left && right) return root;
+
+        return left?left:right;
+
+};
