1aa82b33e6d1b2f4dfdcc9e436920314f4188fed
Ankit Singh <as3824115@gmail.com>
2025-10-30 14:53:04 +0530

    add alt lcs

--- Revision/Dynamic_programming.js
+++ Revision/Dynamic_programming.js
@@ -316,3 +316,20 @@ function longestCommonSubsequence(text1, text2) {
   }
   return dp[n1 - 1][n2 - 1];
 }
+//alternate tabulation LCS
+function longestCommonSubsequence(text1, text2) {
+  const n1 = text1.length, n2 = text2.length;
+  const dp = Array.from({ length: n1+1 }, () => new Array(n2+1).fill(0));
+
+
+  for (let i = 1; i < n1; i++) {
+    for (let j = 1; j < n2; j++) {
+      if (text1[i-1] === text2[j-1]) {
+        dp[i][j] = 1 + dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[n1][n2];
+}
