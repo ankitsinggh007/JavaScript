62b5208e0c83542d948ae47157e1bc3c8e32f780
Ankit Singh <as3824115@gmail.com>
2026-08-30 15:08:44 +0530

    practice:set-7 P-II

--- practice/set-7.js
+++ practice/set-7.js
@@ -71,3 +71,127 @@ var removeNthFromEnd = function (head, n) {
 
   return head;
 };
+//Q find the middle of LL
+function middleNode(head){
+    
+  let fast=head;
+  let slow=head;
+
+    while(fast!==null&&fast.next!==null){
+        fast=fast.next.next;
+        slow=slow.next;
+    }
+    return slow;
+
+};
+//Q check Plaindrom of LL
+let findMiddle=(head1)=>{
+let slow =head1;
+let fast=head1;
+while(fast!==null && fast.next!=null){
+    slow=slow.next;
+    fast=fast.next.next;
+}
+return slow;
+}
+
+let reverseLL=(head)=>{
+    let prev=null;
+    let curr=head;
+    let next=null
+    while(curr!=null){
+        next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
+
+
+
+ function isPalindrome(head: ListNode | null): boolean {
+    let head1=findMiddle(head);
+            head1=reverseLL(head1);
+            
+        let p1=head;
+        let p2=head1;
+            console.log(p1,p2);
+
+    while(p2){
+        if(p1.val!==p2.val) return false;
+        p1=p1.next
+        p2=p2.next
+    }
+    return true;
+
+};
+//find the intersection of two LL
+var getIntersectionNode = function(headA, headB) {
+    let a=headA;
+    let b=headB;
+
+    while(a!=b){
+        a=a?a.next:headB;
+        b=b?b.next:headA;
+    }
+    return a;
+};
+//Q reorder LL
+ let findMid=(head)=>{
+    let fast=head;
+    let slow=head;
+    while(fast && fast.next){
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    let temp=slow.next;
+    slow.next=null;
+    return temp
+ }
+
+ let reverse=(head)=>{
+    let prev=null;
+    let curr=head;
+    while(curr!=null){
+
+        let next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+ }
+
+let reorderList = function(head) {
+    
+let mid=findMid(head);
+let head1=reverse(mid);
+
+let dummy=new ListNode(-1);
+let p1=head;
+let p2=head1;
+let result=dummy;
+
+while(p1 && p2){
+    result.next=p1;
+    p1=p1.next;
+    result=result.next;
+    result.next=p2
+    p2=p2.next;
+    result=result.next
+}
+let temp=p1?p1:p2
+
+while(temp){
+    result.next=temp;
+    temp=temp.next;
+    result=result.next;
+}
+return dummy.next;
+
+
+
+
+return head1;
+};
